349f7ff875c2c394c1e36ba560d4e4a0cfda4812
Jerry <sirusheng@soulapp.cn>
2022-06-14 11:24:52 +0800

    添加编译检查插件-vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    eslint({
+      // 配置选项
+      cache: false // 禁用 eslint 缓存
+    })
   ]
 })
