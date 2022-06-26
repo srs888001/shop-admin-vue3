3b1ab4ff38be92b469604f3f9b566d70160a2fef
Jerry <sirusheng@soulapp.cn>
2022-06-26 17:19:58 +0800

    feat: 添加登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,145 @@
 <template>
-  <h1>登录123</h1>
+  <div class="login-container">
+    <el-form
+      class="login-form"
+      :rules="rules"
+      ref="form"
+      :model="user"
+      size="medium"
+      @submit.prevent="handleSubmit"
+    >
+      <div class="login-form__header">
+        <img
+          class="login-logo"
+          src="@/assets/logo.png"
+          alt="拉勾心选"
+        >
+      </div>
+      <el-form-item prop="account">
+        <el-input
+          v-model="user.account"
+          placeholder="请输入用户名"
+        >
+          <template #prefix>
+            <el-icon><User /></el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="pwd">
+        <el-input
+          v-model="user.pwd"
+          type="password"
+          placeholder="请输入密码"
+        >
+          <template #prefix>
+            <el-icon><Lock /></el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="imgcode">
+        <div class="imgcode-wrap">
+          <el-input
+            v-model="user.imgcode"
+            placeholder="请输入验证码"
+          >
+            <template #prefix>
+              <el-icon><Key /></el-icon>
+            </template>
+          </el-input>
+          <img
+            class="imgcode"
+            alt="验证码"
+            src="https://shop.fed.lagounews.com/api/admin/captcha_pro"
+          >
+        </div>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          class="submit-button"
+          type="primary"
+          :loading="loading"
+          native-type="submit"
+        >
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
 
-<style lang="scss" scoped></style>
+const user = reactive({
+  account: 'admin',
+  pwd: '123456',
+  imgcode: ''
+})
+const loading = ref(false)
+const rules = ref({
+  account: [
+    { required: true, message: '请输入账号', trigger: 'change' }
+  ],
+  pwd: [
+    { required: true, message: '请输入密码', trigger: 'change' }
+  ],
+  imgcode: [
+    { required: true, message: '请输入验证码', trigger: 'change' }
+  ]
+})
+
+const handleSubmit = async () => {
+  console.log('handleSubmit')
+}
+
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  min-width: 400px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2d3a4b;
+}
+
+.login-form {
+  padding: 30px;
+  border-radius: 6px;
+  background: #fff;
+  min-width: 350px;
+  .login-form__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 30px;
+  }
+
+  .el-form-item:last-child {
+    margin-bottom: 0;
+  }
+
+  .login__form-title {
+    display: flex;
+    justify-content: center;
+    color: #fff;
+  }
+
+  .submit-button {
+    width: 100%;
+  }
+
+  .login-logo {
+    width: 90px;
+    height: 74px;
+  }
+  .imgcode-wrap {
+    display: flex;
+    align-items: center;
+    .imgcode {
+      height: 37px;
+    }
+  }
+}
+</style>
